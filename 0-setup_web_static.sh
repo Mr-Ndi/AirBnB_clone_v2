36d29ab56f113f2fe02d7bb0a5bd42e11a8d793f
ndiramiye2 <onlythenotes@gmail.com>
2024-04-03 19:19:24 +0200

    Task 0. Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# A bash script that sets up your web servers for the deployment of web_static.
+
+sudo apt update
+sudo apt install nginx -y
+
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/releases/test/
+
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >> /data/web_static/releases/test/index.hmtl
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# changing the owner to ubuntu
+sudo chown -R ubuntu:ubuntu /data/
+# Update the Nginx configuration to serve
+config_text="location /hbnb_static/ {
+    alias /data/web_static/current/;
+}"
+sudo sed -i "$config_text" /etc/nginx/sites-available/default
+# nginx restsart
+sudo service nginx restart
