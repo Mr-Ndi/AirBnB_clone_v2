037dd80b95cce66a1d034ec5d0b3e96968833d06
ndiramiye2 <onlythenotes@gmail.com>
2024-04-04 19:27:46 +0200

    Nitafanya hivyo peke yangu

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-# Update the system and install Nginx
+#!/usr/bin/env bash
+# updating the system and installing nginx
+
 sudo apt-get update
 sudo apt-get install -y nginx
 
-# Create necessary folders if they don't already exist
+# Create folders required if not already exists
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
@@ -14,19 +15,16 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-
+</html>" >> /data/web_static/releases/test/index.hmtl
 # Create a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Change ownership to ubuntu user and group
+# changing the owner to ubuntu
 sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration to serve the content
+# Update the Nginx configuration to serve
 config_text="location /hbnb_static/ {
     alias /data/web_static/current/;
 }"
-sudo sed -i "/server {/a $config_text" /etc/nginx/sites-available/default
-
-# Restart Nginx
+sudo sed -i "$config_text" /etc/nginx/sites-available/default
+# nginx restsart
 sudo service nginx restart
