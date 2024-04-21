d741f1c70ee7e28b514fefb8c2971f55c3022a06
ndiramiye2 <onlythenotes@gmail.com>
2024-04-21 15:03:34 +0200

    0. Hello Flask!_bwa 3

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+""" Starting a Flask web application """
+
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_word():
+def Say_hello():
+    ''' A function to print hello to Mr HBNB'''
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
