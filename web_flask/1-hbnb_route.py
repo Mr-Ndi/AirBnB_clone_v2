2c60933d95eb5db2268436241d90a29364a47d84
ndiramiye2 <onlythenotes@gmail.com>
2024-04-21 15:14:12 +0200

    Task 1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Starting a Flask web application """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Say_hello():
+    ''' A function to print hello to Mr HBNB'''
+    return 'Hello HBNB!'
+
+
+@app.routes('/hbnb', strict_slashes=False)
+def Displaying():
+    ''' A fuction to show hbnb on hbnb'''
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
